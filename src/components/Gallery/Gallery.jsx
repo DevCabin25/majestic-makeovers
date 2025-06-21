836ac21f68f9f2db1b5dbd2e56f2e31d642f98c1
Asadulla56 <mdasadullahoque294@gmail.com>
2025-06-22 00:42:01 +0600

    Setup Gallery page added

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Gallery = () => {
+    return (
+        <div  className="bg-[##FFFFFF] py-16 px-4 sm:px-8">
+            <div className="text-center mb-10"> 
+                <h1 className="text-4xl font-bold text-[#000000]">Beauty Gallery</h1>
+                <p className="text-gray-600 mt-2">Discover our stunning transformations and artistic creations</p>
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
